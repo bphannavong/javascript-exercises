d29e6d542c4739128ccc075a93c4a3652df64e6e
Billy Phannavong <billy.phannavong@gmail.com>
2022-05-09 17:57:35 -0700

    Finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (!(a >= 0 && b >= 0 && Number.isInteger(a) && Number.isInteger(b))){
+        return 'ERROR';
+    }
+    let sum = 0;
+    let start = (a < b) ? a : b;
+    let end = (a < b) ? b : a;
+    do {
+        sum += start;
+        start++;
+    } while (start <= end);
+    return sum;
 };
 
 // Do not edit below this line
