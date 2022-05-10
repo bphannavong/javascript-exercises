da807f4dc3365b882d059255f21cf67900adebbf
Billy Phannavong <billy.phannavong@gmail.com>
2022-05-09 17:43:19 -0700

    Finish exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...targets) {
+    for (const target of targets){
+        array = array.filter((element) => element !== target);
+    }
+    return array;
 };
 
 // Do not edit below this line
