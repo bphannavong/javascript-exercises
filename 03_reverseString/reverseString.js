0c40574211e3a44af962ebf0476f260486e5b79a
Billy Phannavong <billy.phannavong@gmail.com>
2022-05-09 17:16:22 -0700

    Finish exercises 1-3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const reversedArray = new Array(string.length);
+    for (let i = 0; i <= string.length; i++){
+        reversedArray[i] = string[string.length - i];
+    }
+    return reversedArray.join('');
 };
 
 // Do not edit below this line
