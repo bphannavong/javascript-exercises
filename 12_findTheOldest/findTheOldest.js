bc56276780331350a82064ede273e4289cbd57a5
Billy Phannavong <billy.phannavong@gmail.com>
2022-05-12 19:08:05 -0700

    Finish Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, current) => ((getAge(oldest) > getAge(current)) ? oldest : current));
+}
 
-};
+   
+function getAge(person) {
+    if ('yearOfDeath' in person) {
+        return person.yearOfDeath - person.yearOfBirth;
+    }
+    return (new Date().getFullYear()) - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
