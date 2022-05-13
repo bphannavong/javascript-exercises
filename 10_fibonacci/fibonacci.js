702e7fa3d58dd3dc08230bd1bb54e6436c32df3a
Billy Phannavong <billy.phannavong@gmail.com>
2022-05-12 18:21:12 -0700

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(index) {
+    if (index < 0) {
+        return 'OOPS';
+    }
+    const series = new Array(index);
+    index = index - 1;
+    series[0] = 1;
+    series[1] = 1;
+    for (let i = 2; i <= index; i++){
+        series[i] = series[i-1] + series[i-2];
+    }
+    return series[index];
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
