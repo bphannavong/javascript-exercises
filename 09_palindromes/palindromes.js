6e116896a52040df79ea02a4549a408547fa7125
Billy Phannavong <billy.phannavong@gmail.com>
2022-05-12 18:08:50 -0700

    Finish Exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const noPuncuation = string.replace(/[^\w\s\']|_/g, "").replace(/\s+/g, "").toUpperCase();
+    // const halfLen = Math.floor(noPuncuation.length/2);
+    // for (let i = 0; i <= halfLen; i++) {
+    //     if (noPuncuation[i] !== noPuncuation[noPuncuation.length - 1 - i]){
+    //         return false;
+    //     }
+    // }
+    // return true;
+    return (noPuncuation.split('').reverse().join('') == noPuncuation);
 };
 
 // Do not edit below this line
