a14fc7896491d15e0a59bf33aacecabe5930a777
Billy Phannavong <billy.phannavong@gmail.com>
2022-05-12 17:50:13 -0700

    Finish exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, current) => add(total, current), 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, current) => total * current, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  const array = [];
+	for (let i = 0; i < num; i++){
+    array[i] = i + 1;
+  }
+  return multiply(array);
 };
 
 // Do not edit below this line
